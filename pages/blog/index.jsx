37733e6ccc15b24ce296f83893a649cfcb1daf78
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-05-09 14:44:36 +0700

    membuat query parameter

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -2,10 +2,19 @@
 // => Untuk mengaksesnya localhost:3000/blog 
 // => yang dibaca index.js dianggap (/), sama seperti index.js didalam folder pages
 
+// > import package useRouter dari next/router
+import { useRouter } from "next/router";
+
 const Blog = () => {
+  // > inisialisasi method useRouter dan assign ke variable router
+  const router = useRouter();
+  // > Menangkap query parameter yang dikirim oleh user
+  const { page, limit } = router.query;
+
   return (
     <>
       <h1>Ini adalah halaman blog</h1>
+      <p>Query Parameter: { page } Page dan { limit } Limit </p>
     </>
   );
 }
