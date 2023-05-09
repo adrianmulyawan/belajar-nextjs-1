6c047c4fa438e0ffacb6d10f2eba36b0b715ed8c
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-05-09 19:33:54 +0700

    tambah komentar untuk SSG getStaticProps

--- pages/products/index.jsx
+++ pages/products/index.jsx
@@ -22,22 +22,31 @@ const Product = ({ products }) => {
 
 export default Product;
 
+// 1.) getStaticProps
 // > Untuk membuat SSG (Static Site Generation)
-// => kita bisa membuat function getStaticProps()
-// => getStaticProps digunakan bila menggunakan data eksternal (API)
-
+// => kita bisa membuat function getStaticProps().
+// => getStaticProps digunakan bila menggunakan data eksternal (API).
+// => Digunakan bila konten pada halama bergantung pada data eksternal.
 // > Method ini berjalan disisi server
-
 // > Penting
 // # getStaticProps digunakan untuk membuat SSG. 
 // # Dengan kata lain next js akan membuat static html dan static data dalam format json pada saat build time.
 // # meskipun kita berkomunikasi dengan API.
 // # Saat kita build ke production, kemudian kita ubah data API-nya data yang diproduction tidak akan berubah (tidak berkomunikasi langsung dengan API).
 export const getStaticProps = async () => {
+  // > Consume API
   const res = await axios('http://localhost:3004/products');
   const data = await res.data;
   // console.info(data, '=> Data dari server');
 
+  // > Kondisi bila data tidak ditemukan
+  if (!data) {
+    return {
+      notFound: true
+    }
+  }
+
+  // > Kondisi bila data ditemukan
   return {
     props: {
       products: data
